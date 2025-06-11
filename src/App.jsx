a2b1fae1471a9e370d1fa8e32496ea20375754b4
Emin Basbayan <emin.basbayan@icloud.com>
2025-06-11 10:24:11 +0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import Products from './Products';
 
 function App() {
+  const [state, setState] = useState("Emre");
+  //   let customerName = 'Emre';
+
+  function handleChange() {
+    // customerName = 'Ahmet';
+
+    // state = "Ahmet" --- yanlış
+
+    setState('Ahmet');
+  }
+
+  console.log("re-rendered!");
+  
+
+
   return (
     <div>
       <h1>App Component</h1>
-      <Products />
+      <p>{state}</p>
+      <button onClick={handleChange}>İsmi Değiştir!</button>
+      {/* <Products /> */}
     </div>
   );
 }
